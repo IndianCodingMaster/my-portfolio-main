66f62361132259bf9d68c676f88fe53f24b190a6
Satya Prakash <satyaprakash91180@gmail.com>
2024-03-06 02:02:59 +0530

    first project added

---
+++ src/components/Project.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import weatherapp from "../assets/weather-app.png";
+
+const Project = () => {
+  return (
+    <div className=" flex justify-center items-center flex-wrap gap-7">
+      <div className="sm:max-w-[600px] max-w-[400px]  p-5 rounded-2xl shadow-xl">
+        <img
+          className="shadow-xl hover:scale-95 delay-75 transition-all ease-in"
+          src={weatherapp}
+          alt=""
+        />
+        <div className="mt-4 flex justify-around items-center gap-4">
+          <button className="hover:bg-blue-600 hover:text-white w-[7rem] sm:text-sm md:text-xl border-[2px] border-blue-600 px-4 py-2.5 rounded-full delay-75 transition-all ease-in hover:scale-110">
+            <a href="https://github.com/IndianCodingMaster/Weather-App">
+              GitHub
+            </a>
+          </button>
+          <button className="hover:bg-blue-600 hover:text-white  w-[7rem] sm:text-sm md:text-xl border-[2px] border-blue-600 px-4 py-2.5 rounded-full delay-75 transition-all ease-in hover:scale-110">
+            <a href="https://weather-app-satya.vercel.app/">Live Demo</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Project from "./Project";
 
 const Projects = () => {
   return (
     <div
       id="projects"
-      className="py-28 px-16 m-auto bg-[#F9F9F9] text-center text-2xl font-bold"
+      className="py-14 px-16 m-auto bg-[#F9F9F9] text-center text-2xl font-bold"
     >
-      <h4>New projects coming soon.. 🤠</h4>
+      <h4 className="text-3xl mb-5 text-blue-600">Projects 🤠</h4>
+      <Project />
     </div>
   );
 };
