5624d331109087a72418320f7ccc25a3cd81f91e
Satya Prakash <satyaprakash91180@gmail.com>
2024-03-07 00:08:16 +0530

    .

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import weatherapp from "../assets/weather-app.png";
+import weatherapp from "../assets/weather-app2.png";
 import imageslider from "../assets/image-slider.png";
 
 const Project = () => {
   return (
     <div className=" flex justify-center items-center flex-wrap gap-7">
-      <div className="p-5 rounded-2xl shadow-xl w-[400px] h-[570px]">
+      <div className="p-5 rounded-2xl shadow-xl w-[400px] md:h-[540px]">
         <img
-          className="shadow-xl hover:scale-95 delay-75 transition-all ease-in w-[100%] h-[90%] object-cover rounded-lg"
+          className="shadow-xl hover:scale-95 delay-75 transition-all ease-in sm:w-[100%] md:h-[90%] object-cover rounded-lg"
           src={weatherapp}
           alt=""
         />
-        <div className="mt-4 flex justify-evenly items-center gap-4">
+        <div className="mt-3 flex justify-evenly items-center gap-4">
           <button className="hover:bg-blue-600 hover:text-white md:w-[8rem] sm:text-sm border-[2px] border-blue-600 px-4 py-2.5 rounded-full delay-75 transition-all ease-in hover:scale-110">
             <a href="https://github.com/IndianCodingMaster/Weather-App">
               GitHub
@@ -22,13 +22,13 @@ const Project = () => {
           </button>
         </div>
       </div>
-      <div className="p-5 rounded-2xl shadow-xl w-[400px] h-[570px]">
+      <div className="p-5 rounded-2xl shadow-xl w-[400px] h-[540px]">
         <img
-          className="shadow-xl hover:scale-95 delay-75 transition-all ease-in w-[100%] h-[90%] object-cover rounded-lg"
+          className="shadow-xl hover:scale-95 delay-75 transition-all ease-in sm:w-[100%] h-[90%] object-cover rounded-lg"
           src={imageslider}
           alt=""
         />
-        <div className="mt-4 flex justify-evenly items-center gap-4">
+        <div className="mt-3 flex justify-evenly items-center gap-4">
           <button className="hover:bg-blue-600 hover:text-white md:w-[8rem] sm:text-sm border-[2px] border-blue-600 px-4 py-2.5 rounded-full delay-75 transition-all ease-in hover:scale-110">
             <a href="https://github.com/IndianCodingMaster/Image-Slider">
               GitHub
