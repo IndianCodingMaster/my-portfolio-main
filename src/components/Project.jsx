60778861fc964693f096693d60b96ac1a4e47305
Satya Prakash <satyaprakash91180@gmail.com>
2024-03-12 23:10:45 +0530

    Add third project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import weatherapp from "../assets/weather-app2.png";
 import imageslider from "../assets/image-slider.png";
+import restApi from "../assets/rest-api.png";
 
 const Project = () => {
   return (
@@ -36,6 +37,23 @@ const Project = () => {
           </button>
           <button className="hover:bg-blue-600 hover:text-white md:w-[8rem] sm:text-sm border-[2px] border-blue-600 px-4 py-2.5 rounded-full delay-75 transition-all ease-in hover:scale-110">
             <a href="https://image-slider-satya.vercel.app/">Live Demo</a>
+          </button>
+        </div>
+      </div>
+      <div className="p-5 rounded-2xl shadow-xl w-[400px] h-[540px]">
+        <img
+          className="shadow-xl hover:scale-95 delay-75 transition-all ease-in sm:w-[100%] h-[90%] object-cover rounded-lg"
+          src={restApi}
+          alt=""
+        />
+        <div className="mt-3 flex justify-evenly items-center gap-4">
+          <button className="hover:bg-blue-600 hover:text-white md:w-[8rem] sm:text-sm border-[2px] border-blue-600 px-4 py-2.5 rounded-full delay-75 transition-all ease-in hover:scale-110">
+            <a href="https://github.com/IndianCodingMaster/Rest-Countries-Api">
+              GitHub
+            </a>
+          </button>
+          <button className="hover:bg-blue-600 hover:text-white md:w-[8rem] sm:text-sm border-[2px] border-blue-600 px-4 py-2.5 rounded-full delay-75 transition-all ease-in hover:scale-110">
+            <a href="https://my-rest-countries-api.vercel.app/">Live Demo</a>
           </button>
         </div>
       </div>
